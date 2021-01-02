d7cef08b2cd0490af17a0a89e8ad0d96eb45ca97
Sirah Matrix <70299982+KeysorSoze@users.noreply.github.com>
2021-01-01 21:02:25 -0700

    added items
    -added coal nugget
    -added charcoal nugget
    -added diamond shard
    -added emerald shard

--- src/main/java/com/keysorsoze/moreParts/MoreParts.java
+++ src/main/java/com/keysorsoze/moreParts/MoreParts.java
@@ -1,11 +1,14 @@
 package com.keysorsoze.moreParts;
 
+import com.keysorsoze.moreParts.regestry.ItemRegistery;
 import net.fabricmc.api.ModInitializer;
 
-
 public class MoreParts implements ModInitializer {
+
+    public static final String MOD_ID = "kmp";
+
     @Override
     public  void  onInitialize(){
-
+        ItemRegistery.register();
     }
 }
